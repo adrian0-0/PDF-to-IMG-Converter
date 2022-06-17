e3af9bd813d1328e922bfeed4a3e7c72ba4cbf8e
Yuri Goytacaz <yurigoytacaz@gmail.com>
2022-06-17 18:19:05 -0300

    fix conflict

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-<h1>Conversor PDF-IMG com compatibilidade com fontes</h1>
+<h1>Conversor PDF para IMG com suporte a fontes personalizadas</h1>
 
-<p><h2>Bibliotecas usadas:</h2></p>
-<p><a href = "https://github.com/jsdom/jsdom#readme">jsdom</a></p>
-<p><a href = "https://github.com/kb47/pdf-poppler#readme">pdf-poppler</a></p>
+<h2>Bibliotecas usadas:</h2>
+<a href="https://github.com/jsdom/jsdom#readme">jsdom</a>
+<a href="https://github.com/kb47/pdf-poppler#readme">pdf-poppler</a>
 
-<p><h3>Modo de uso:</h3></p>
-<p>node index.js ./assets/cabana.pdf jpeg</p>
+<h3>Modo de uso:</h3>
+<code>node convert.js ./sample.pdf jpeg</code>
 <ul>
-    <li><p>node argument: diretório do path</p></li>
-    <li><p>node argument: formato da imagem png ou jpeg</p></li>
+    <li>node argument: filename</li>
+    <li>node argument: extensão da imagem (png ou jpeg)</li>
 <ul>
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:index.js
 const pdf = require('pdf-poppler');
 const jsdom = require("jsdom");
 const fs = require('fs');
@@ -113,3 +114,118 @@ function create__html() {
 }
 
 renomear(_terminal__arg)
+=======
+const pdf = require('pdf-poppler');
+const jsdom = require("jsdom");
+const fs = require('fs');
+const { JSDOM } = jsdom;
+
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cárdapio Cabana do Sol</title>
+</head>
+<body>
+
+</body>
+</html>
+`
+let dom = new JSDOM(`${htmlContent}`);
+let document = dom.window.document;
+let c =0
+let i = 1;
+let _page__array = 0;
+
+let data = new Date()
+let month = (data.getMonth() + 1).toString().padStart(2, "0");
+let day = data.getDate().toString().padStart(2, "0");
+let cache_data = day+month
+
+
+// Recebe os args inputados no terminal (path do diretório / formato da imagem)
+let _terminal__arg = process.argv;
+_terminal__arg.forEach(function terminal__arg (input, index) {
+    console.log(`${index}: ${input}`)
+});
+
+//Renomeia os arquivos de imagem e o path inbutido
+function renomear(terminal__arg) {
+    let file = terminal__arg[2]    
+    let file__rename = file.split(/[ ./\r\n/\\]+/)
+    let pdf__name = file__rename.slice(-2) [0];
+    
+    convert(terminal__arg, file, pdf__name); 
+
+}
+
+// Converte o diretório do pdf as para o formato selecionado 
+function convert(terminal__arg, file, pdf__name) {
+
+// Cria a folder padrão
+    fs.mkdir('./dist', { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+
+// Declaraçôes do path e formato da imagem
+    let opts = {
+        format: terminal__arg[3],
+        out_dir: "./dist",
+        out_prefix: pdf__name,
+        page: null,
+        scale: 3000
+    }
+
+    pdf.convert(file, opts)
+    .then(res => {
+        console.log('Conversão concluida!');
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+//Envia info da remomeação para a edição do html 
+    pdf.info(file)
+    .then(pdfinfo => {
+        _page__array = pdfinfo.pages
+        edit__html(_page__array, pdf__name, terminal__arg[3], cache_data);
+    });
+    
+} 
+
+//Realiza alteraçôes no html
+function edit__html(pages, pdf__name, format, cache_data) {
+
+    if  (pages > 9) {
+        for (c; c<pages; c++) {
+            let page_count = i.toString().padStart(2, "0");
+            document.querySelector("body").innerHTML += (`
+            <img src="${page_count}.${format}?=${cache_data}" alt="" style="width: 100%; max-width: none;"><br>`);
+            i++;
+        }   
+    }
+
+    else if (pages <= 9) {
+        for (c; c<pages; c++) {
+            document.querySelector("body").innerHTML += (`
+            <img src="${i}.${format}?=${cache_data}" alt="" style="width: 100%; max-width: none;"><br>`);
+            i++;
+        }        
+    }
+    create__html();                 
+}
+
+
+//Realiza a escrita do html
+function create__html() {
+    fs.writeFile("./dist/index.html", document.documentElement.innerHTML, function(error) {
+        console.log(document.documentElement.innerHTML)
+        if (error) throw error;
+    });
+}
+
+renomear(_terminal__arg)
+>>>>>>> 8cae729 (corrige pasta de saide e nome das imagens):convert.js
