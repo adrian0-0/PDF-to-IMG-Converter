0a59f3122fc1dabed04551a830f40f5b17381376
Adrian Vinicius Roma Alves <adrian@maximize.art.br>
2022-06-24 19:04:37 -0300

    feat: Adciona configuranções para cabana e emporio via json file

--- convert.js
+++ convert.js
@@ -17,6 +17,7 @@ const htmlContent = `
 `
 let dom = new JSDOM(`${htmlContent}`);
 let document = dom.window.document;
+let pdf_name = "page"
 let c =0
 let i = 1;
 let _page_array = 0;
@@ -24,27 +25,35 @@ let _page_array = 0;
 let data = new Date()
 let month = (data.getMonth() + 1).toString().padStart(2, "0");
 let day = data.getDate().toString().padStart(2, "0");
-let cache_data = day+month
-
+let cache_data = day+month;
 
 // Recebe os args inputados no terminal (path do diretório / formato da imagem)
 let _terminal_arg = process.argv;
 _terminal_arg.forEach(function terminal__arg (input, index) {
     console.log(`${index}: ${input}`)
-});
-//page
-//Renomeia os arquivos de imagem e o path inbutido
-function renomear(terminal__arg) {
-    let file = terminal__arg[2]    
-    let file__rename = file.split(/[ ./\r\n/\\]+/)
-    let pdf_name = file__rename.slice(-2) [0];
     
-    convert(terminal__arg, file, pdf_name); 
+});
 
+function dataToConversion(terminal__arg) {
+    fs.readFile('dataToConversion.json', (err, data) => {
+        if (err) throw err;
+        
+        jsonData = JSON.parse(data);
+        terminal__arg[3].toLowerCase()
+
+        Object.keys(jsonData.stores).forEach(key => {
+            if (key == terminal__arg[3]) {
+                convert(terminal__arg, key)
+
+            };           
+          });
+      });
 }
 
 // Converte o diretório do pdf as para o formato selecionado 
-function convert(terminal__arg, file, pdf_name) {
+function convert(terminal__arg, index) {
+    let storeData = jsonData.stores[index];
+    console.log(storeData.formato);
 
 // Cria a folder padrão
     fs.mkdir('./dist', { recursive: true }, (err) => {
@@ -53,14 +62,14 @@ function convert(terminal__arg, file, pdf_name) {
 
 // Declaraçôes do path e formato da imagem
     let opts = {
-        format: terminal__arg[3],
+        format: storeData.formato,
         out_dir: "./dist",
         out_prefix: pdf_name,
         page: null,
-        scale: terminal__arg[4]
+        scale: storeData.scale
     }
 
-    pdf.convert(file, opts)
+    pdf.convert(terminal__arg[2], opts)
     .then(res => {
         console.log('Conversão concluida!');
     })
@@ -69,16 +78,16 @@ function convert(terminal__arg, file, pdf_name) {
     })
 
 //Envia info da remomeação para a edição do html 
-    pdf.info(file)
+    pdf.info(terminal__arg[2])
     .then(pdfinfo => {
         _page_array = pdfinfo.pages
-        edit__html(_page_array, pdf_name, terminal__arg[3], cache_data, opts.out_dir);
+        edit__html(_page_array, storeData.formato, cache_data, opts.out_dir);
     });
     
 } 
 
 //Realiza alteraçôes no html
-function edit__html(pages, pdf_name, format, cache_data, standard_folder) {
+function edit__html(pages, format, cache_data, standard_folder) {
 
     if  (pages > 9) {
         for (c; c<pages; c++) {
@@ -108,4 +117,4 @@ function create__html(standard_folder) {
     });
 }
 
-renomear(_terminal_arg)
+dataToConversion(_terminal_arg)
