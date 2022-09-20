eabc97474c5edb085235206d65c2b907f0bb5423
AdrianVinicius <adrian@maximize.art.br>
2022-09-20 15:02:39 -0300

    feat: dimensionamento do HTML

--- www/src/App.jsx
+++ www/src/App.jsx
@@ -6,23 +6,35 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Conversor PDF-IMG</h1>
-      <div className="app__button">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-      </div>
-      <div className='app__pdf'>
+      <header>
+        <div className='header'></div>
+      </header>
+      <main>
+        <div className='main'>
+          <h1>Conversor PDF-IMG</h1>
+          <div className="app__button">
+            {/* <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button> */}
+          </div>
+          <div className='app__convert-opts'>
+            <h2>Opções de conversão</h2>
+            <div><p>Digite o DPI</p></div>
+            <div><input type="number" className='app__input' placeholder='DPI recomendado 300'/></div>
+        
+            <label htmlFor="extensão">Selecione um formato de imagem: </label>
+            <select name="extensão" placeholder='Selecione o formato da imagem'>
+              <option value="jpg">JPG</option>
+              <option value="png">PNG</option>
+            </select>
+        
+          </div>
+          <div className='app__pdf'>
+            <button type='submit'>Selecionar Arquivo PDF</button>
+          </div>
+        </div>
+      </main>
 
-      </div>
-      <div className='app__convert-opts'>
-        <h2>Opções de conversão</h2>
-        <div><p>Digite o DPI</p></div>
-        <div><input type="number" className='app__input' placeholder='DPI recomendado 300'/></div>
-        <div><p>Selecione o formato da imagem</p></div>
-        <div>PNG <input type="checkbox" /></div>
-        <div>JPEG <input type="checkbox" /></div>
-      </div>    
     </div>
   )
 }
