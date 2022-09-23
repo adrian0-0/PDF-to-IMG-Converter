3d411610c33239bacea4efc2cad1a4127bad80de
AdrianVinicius <adrian@maximize.art.br>
2022-09-23 14:45:11 -0300

    fix: vite não sendo reconhecido

--- Dockerfile
+++ Dockerfile
@@ -21,8 +21,8 @@ RUN yarn
 
 COPY . .
 
-# ENV PORT=8080
+ENV PORT=8080
 
-# EXPOSE 8080
+EXPOSE 8080
 
 CMD [ "yarn", "dev" ]
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // const app = require ('express')();
 import express from "express";
 const app = express();
-app.get('/', (req, res) => 
-    res.send()
+app.get("/", (req, res) => res.send());
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>
+  console.log(`app listening on http://localhost:${port}`)
 );
-
-const port = process.env.PORT || 3333;
-
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`))
