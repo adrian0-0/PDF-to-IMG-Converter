d2809c6abe668f37c029a5bc8961403a56d415c4
AdrianVinicius <adrian@maximize.art.br>
2022-09-26 16:36:21 -0300

    fix: Quebra da UI

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// const app = require ('express')();
-import express from "express";
-const app = express();
-app.get("/", (req, res) => res.send());
+// // const app = require ('express')();
+// import express from "express";
+// const app = express();
+// app.get("/", (req, res) => res.send());
 
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 8080;
 
-app.listen(port, () =>
-  console.log(`app listening on http://localhost:${port}`)
-);
+// app.listen(port, () =>
+//   console.log(`app listening on http://localhost:${port}`)
+// );
--- www/src/App.jsx
+++ www/src/App.jsx
@@ -14,36 +14,38 @@ function App() {
       <main>
         <div className="main">
           <div className="app__block">
-            <div className="app__background">
-              <h1>Conversor PDF-IMG</h1>
-              <div className="main__button">
-                {/* <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-                </button> */}
-              </div>
-              <div className="main__convert-opts">
-                <h2>Opções de conversão</h2>
-                <Range></Range>
-                <label htmlFor="extensão">
-                  Selecione um formato de imagem:{" "}
-                </label>
-                <select
-                  name="extensão"
-                  placeholder="Selecione o formato da imagem"
-                >
-                  <option value="jpg">JPG</option>
-                  <option value="png">PNG</option>
-                </select>
-              </div>
-              <div className="main__pdf">
-                <div className="main__pdf--submit">
-                  <Button
-                    variant="outline-primary"
-                    type="submit"
-                    className="button"
+            <div className="app__block--child">
+              <div className="app__background">
+                <h1>Conversor PDF-IMG</h1>
+                <div className="main__button">
+                  {/* <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                  </button> */}
+                </div>
+                <div className="main__convert-opts">
+                  <h2>Opções de conversão</h2>
+                  <Range></Range>
+                  <label htmlFor="extensão">
+                    Selecione um formato de imagem:{" "}
+                  </label>
+                  <select
+                    name="extensão"
+                    placeholder="Selecione o formato da imagem"
                   >
-                    Selecionar Arquivo PDF
-                  </Button>
+                    <option value="jpg">JPG</option>
+                    <option value="png">PNG</option>
+                  </select>
+                </div>
+                <div className="main__pdf">
+                  <div className="main__pdf--submit">
+                    <Button
+                      variant="outline-primary"
+                      type="submit"
+                      className="button"
+                    >
+                      Selecionar Arquivo PDF
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
