a3d79ae1913abc6694b962e1c95dae231eae92e5
Adrian Vinicius <adrianromaalves@hotmail.com>
2023-01-25 21:43:37 -0300

    fix: docker autoupdate with ViteHMR

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 8080,
+    watch: {
+      usePolling: true
+    },
     
     proxy: {
       "/api": "http://localhost:5000/",
